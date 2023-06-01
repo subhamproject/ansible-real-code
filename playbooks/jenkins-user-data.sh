88b05611723153936f01c04a7b70eb2ff81fb6a8
subhamproject <subhammandal01@gmail.com>
2023-06-01 16:57:34 +0000

    added playbook for jenkins

---
+++ playbooks/jenkins-user-data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
